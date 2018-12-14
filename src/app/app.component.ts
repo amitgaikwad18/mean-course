c6c79e5b63cb94532d2e818918232dd64f3f0fde
Amit GAIKWAD <Amit.GAIKWAD@firmenich.com>
2018-12-14 16:28:59 +0530

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { Post } from './posts/post.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mean-course';
+
+  addedPosts: Post[] = [];
+
+  onPostAdded(post) {
+    this.addedPosts.push(post);
+  }
+}
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+import { Post } from '../post.model';
+
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+
+  // posts = [
+  //   {title: 'First Post', content: 'First Post Content'},
+  //   {title: 'Second Post', content: 'Second Post Content'},
+  //   {title: 'Third Post', content: 'Third Post Content'},
+  // ];
+
+  @Input() posts: Post[] = [];
+}
