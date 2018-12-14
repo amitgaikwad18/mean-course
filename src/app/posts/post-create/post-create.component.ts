1531025838290d97d5fa5833fcd39de1279eefd3
Amit GAIKWAD <Amit.GAIKWAD@firmenich.com>
2018-12-14 17:21:45 +0530

    added mat-error, ngModel, ngForm, form

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
 import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -15,9 +16,13 @@ export class PostCreateComponent {
 
   @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
+  onAddPost(form: NgForm) {
 
-    const post: Post = {title: this.enteredTitle, content: this.enteredContent};
+    if (form.invalid) {
+      return;
+    }
+
+    const post: Post = {title: form.value.title, content: form.value.content};
     this.postCreated.emit(post);
   }
 
